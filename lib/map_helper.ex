d7f6b96d5be4eac2aa2531b43f3e2c74254d6683
Eddy de Boer <eddy147@gmail.com>
2021-03-07 14:49:42 +0100

    Working...used http://www.johnpdaigle.com/complexable/2017/07/02/recursive-transforms-on-maps-on-elixir.html

--- lib/map_helper.ex
+++ lib/map_helper.ex
@@ -1,18 +1,21 @@
 defmodule MapHelper do
   @moduledoc """
-  Documentation for `MapHelper`.
+  Handy tools if you work with a map.
   """
-
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MapHelper.hello()
-      :world
-
+  Removes tuples where the value is empty
   """
-  def hello do
-    :world
+  def removeEmpty(%{} = map) when is_map(map) do
+    compactor = fn({k, v}, acc) ->
+      cond do
+        is_map(v) and Map.has_key?(v, :__struct__) ->
+          Map.put_new(acc, k, v)
+        is_map(v) and Enum.empty?(v) -> acc
+        is_map(v) -> Map.put_new(acc, k, removeEmpty(v))
+        is_nil(v) -> acc
+        true -> Map.put_new(acc, k, v)
+      end
+    end
+    Enum.reduce(map, %{}, compactor)
   end
 end
--- test/map_helper_test.exs
+++ test/map_helper_test.exs
@@ -2,7 +2,24 @@ defmodule MapHelperTest do
   use ExUnit.Case
   doctest MapHelper
 
-  test "greets the world" do
-    assert MapHelper.hello() == :world
+  test "do nothing if no empty values found" do
+    result = MapHelper.removeEmpty(%{})
+    assert result == %{}
+  end
+
+  test "do nothing if the map does not have any empty values" do
+    map = %{category: "45", code: "YS1U"}
+    result = MapHelper.removeEmpty(%{category: "45", code: "YS1U"})
+    assert result == map
+  end
+
+  test "remove an empty value" do
+    result = MapHelper.removeEmpty(%{category: "45", code: nil, date: "2021-04-01"})
+    assert result == %{category: "45", date: "2021-04-01"}
+  end
+
+  test "remove an empty value multiple level map" do
+    result = MapHelper.removeEmpty(%{product: %{category: "45", code: nil}, date: "2021-04-01"})
+    assert result == %{product: %{category: "45"}, date: "2021-04-01"}
   end
 end
